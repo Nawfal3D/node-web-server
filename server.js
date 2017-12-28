e175905e856e2e47210e783536d8b88282106ed2
Nawfal Al Hashimy <nawfal@ecs-mac-mar-4.home>
2017-12-28 19:42:44 +0000

    Setup, Start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -23,9 +24,9 @@ app.use((req,res,next)=>{
   next();
 });
 
-app.use((req,res,next)=>{
-  res.render('maintenance.hbs');
-});
+// app.use((req,res,next)=>{
+//   res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname +`/public`));
 
@@ -42,13 +43,6 @@ app.get('/about',(req,res)=>{
   })
 });
 
-// app.get('/maintenance',(req,res)=>{
-//   res.render('maintenance.hbs',{
-//     pageTitle: 'Maintenance Page',
-//     wellcomeMessage: 'Sorry closed for maintainace'
-//   })
-// });
-
 app.get('/bad',(req,res)=>{
   res.send({
     errorMessage: 'Unable to handle request'
@@ -56,4 +50,6 @@ app.get('/bad',(req,res)=>{
 });
 
 
-app.listen(3000,()=>{console.log('server is up on port 3000');});
+app.listen(port,()=>{
+  console.log(`server is up on port ${port}`);
+});
